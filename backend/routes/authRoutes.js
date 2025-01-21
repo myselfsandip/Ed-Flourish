7e64c9018f926a1585ceb6de89177eaf42514b06
myselfsandip <sandipsingha714@gmail.com>
2025-01-22 02:07:54 +0530

    Design Updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { login, register, verifyEmail, logout, forgotPassword, resetPassword, checkAuth , changePassword} from "../controllers/authControllers.js";
+import { login, register, verifyEmail, logout, forgotPassword, resetPassword, checkAuth } from "../controllers/authControllers.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
 
@@ -11,7 +11,6 @@ router.post('/verify_email', verifyEmail);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/forgot_password", forgotPassword);
-router.post("/change_password", changePassword);
 router.post("/reset_password", resetPassword);
 router.post("/check_auth", verifyToken, checkAuth);
 
